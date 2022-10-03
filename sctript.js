59e7e42bafa3680aff4f8dd05d9fb198cc0f437a
Koporson <108631492+K1337xz@users.noreply.github.com>
2022-10-03 22:19:30 +0200

    da

---
+++ sctript.js
@@ -0,0 +1,6 @@
+const dataTheme = document.querySelector("html");
+const darkModeInput = document.querySelectorAll(".switch");
+
+darkModeInput.addEventListener("click", () => {
+	console.log("chuj");
+});
